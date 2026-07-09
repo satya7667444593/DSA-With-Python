cc95a526548e59fd12de3f808b0ee9b5671379a1
Satya Prakash Verma <satyaprakashverma82942@gmail.com>
2026-07-09 21:44:01 +0530

    sp

---
+++ quick_sort.py
@@ -0,0 +1,9 @@
+n= int (input())
+mylist = [int(ele) for ele in input().split()]
+
+def quick_sort(arr, low, high):
+    if low < high:
+        pivot_index = partition(arr, low, high)
+        quick_sort(arr, low, pivot_index - 1)
+        quick_sort(arr, pivot_index + 1, high)
+        
